efc412702ae09ae9e934476320618f6ecaecde85
ekkosun <178380101@qq.com>
2023-04-13 09:49:03 +0800

    新加了一个压缩的bat脚本，不过要先下载bandizip压缩软件。

---
+++ zip.bat
@@ -0,0 +1,16 @@
+@echo off
+set zip_exe=bz.exe
+set code_zip_dir=zips
+if exist %code_zip_dir% (
+    echo zipping~
+) else (
+    md %code_zip_dir%
+)
+set curtime=%time:~0,2%-%time:~3,2%
+
+:delleft
+if "%curtime:~0,1%"==" " set curtime=%curtime:~1%&&goto delleft
+
+set filename=src-%curtime%.zip
+echo %filename%
+%zip_exe% c %code_zip_dir%/%filename% ./src/*h ./src/*cpp ./src/CMakeLists.txt
